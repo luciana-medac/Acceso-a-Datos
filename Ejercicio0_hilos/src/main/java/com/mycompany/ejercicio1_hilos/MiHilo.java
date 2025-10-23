11594f790eb16cfac9f6b63d67253aaecb75366d
lachthachi <lmg0043@alu.medac.es>
2025-10-23 09:09:20 +0200

    se han cambiado al repositorio correspondiente

--- Ejercicio0_hilos/src/main/java/com/mycompany/ejercicio1_hilos/MiHilo.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.ejercicio1_hilos;
-
-public class MiHilo extends Thread{
-    
-    public void run(){
-        System.out.println("Ejecutando en un hilo: " + getName());
-    }
-
-}
