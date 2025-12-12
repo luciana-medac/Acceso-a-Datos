59acc926a83ba9798d63420f7c30f8bdc3870fd1
lachthachi <lmg0043@alu.medac.es>
2025-12-12 11:27:30 +0100

    BDOR para la práctica del tema 10

---
+++ BDOR/Concesionario.sql
@@ -0,0 +1,176 @@
+DROP TYPE VehiculoType FORCE;
+DROP TYPE MarcaType FORCE;
+DROP TYPE CocheType FORCE;
+
+DROP TABLE Marca CASCADE CONSTRAINTS;
+DROP TABLE Vehiculo CASCADE CONSTRAINTS;
+DROP TABLE Coche CASCADE CONSTRAINTS;
+
+-- Crear un tipo de objeto llamado VehiculoType
+CREATE OR REPLACE TYPE VehiculoType AS OBJECT(
+
+    IdVehiculo NUMBER,
+    nombre VARCHAR2(30)
+
+) NOT FINAL; -- Permite que más objetos hereden de vehiculo
+
+CREATE OR REPLACE TYPE MarcaType AS OBJECT(
+    
+    IdMarca NUMBER,
+    nombre VARCHAR2(30)
+    
+);
+
+-- Creamos el VARRAY
+CREATE OR REPLACE TYPE ColoresType AS VARRAY(3) OF VARCHAR2(20);
+
+-- Tabla anidada
+CREATE OR REPLACE TYPE ReparacionesType as TABLE OF VARCHAR(50);
+
+CREATE OR REPLACE TYPE CocheType UNDER VehiculoType(
+    
+    precio NUMBER,
+    marca REF MarcaType,
+    colores ColoresType, -- Implementamos el varray
+    reparaciones ReparacionesType
+
+);
+
+CREATE TABLE Marca OF MarcaType(
+    IdMarca primary key
+);
+
+CREATE TABLE Coche OF CocheType(
+    IdVehiculo primary key,
+    SCOPE FOR(marca) IS Marca -- le dice a Oracle que todas las referencias marca apunten a filas de la tabla Marca
+)
+NESTED TABLE reparaciones STORE AS Reparaciones_Store; -- Hace referencia a la tabla de reparaciones (tabla anidada)
+
+-- AÑADIR DATOS
+
+INSERT INTO Marca VALUES (MarcaType(1,'Luciana_Seat'));
+INSERT INTO Marca VALUES (MarcaType(2,'Luciana_Toyota'));
+INSERT INTO Marca VALUES (MarcaType(3,'Luciana_Renault'));
+INSERT INTO Marca VALUES (MarcaType(4,'Luciana_Cupra'));
+INSERT INTO Marca VALUES (MarcaType(5,'Luciana_Citroen'));
+
+INSERT INTO Coche 
+    SELECT CocheType(
+        1,
+        'Ibiza',
+        15000,
+        REF(m),
+        ColoresType('Rojo', 'Blanco'),
+        ReparacionesType('Cambio aceite', 'Pastillas freno')
+    ) FROM Marca m WHERE m.idmarca = 1;
+
+INSERT INTO Coche
+    SELECT CocheType(
+        2,
+        'Yaris',
+        20000,
+        REF(m),
+        ColoresType('Negro', 'Rojo'),
+        ReparacionesType('Cambio de aceite', 'Cambio ruedas')
+    ) FROM Marca m WHERE m.idmarca = 2;
+
+INSERT INTO Coche
+    SELECT CocheType(
+        3,
+        'Clio',
+        10000,
+        REF(m),
+        ColoresType('Naranja'),
+        ReparacionesType('Cambio filtro', 'Revision general')
+    ) FROM Marca m WHERE m.idmarca = 3;
+    
+INSERT INTO Coche
+    SELECT CocheType(
+        4,
+        'Ateca',
+        30000,
+        REF(m),
+        ColoresType('Rojo Cereza'),
+        ReparacionesType('Revision general')
+    ) FROM Marca m WHERE m.idmarca = 4;
+
+INSERT INTO Coche
+    SELECT CocheType(
+        5,
+        'C3',
+        12000,
+        REF(m),
+        ColoresType('Azul'),
+        ReparacionesType('Limpialunas nuevo')
+    ) FROM Marca m WHERE m.idmarca = 5;
+
+
+-- Ver todas las marcas
+SELECT * FROM Marca;
+
+-- Ver todos los coches
+SELECT * FROM Coche;
+
+-- Ver coches con nombre de a marca (usando DEREF)
+SELECT 
+    c.idvehiculo,
+    c.nombre,
+    c.precio,
+    DEREF(c.marca).idMarca AS idMarca,
+    DEREF(c.marca).nombre AS NombreMarca
+FROM Coche c;
+    
+-- Ver coches con nombre de marca ordenados por el nombre de la marca
+SELECT 
+    c.idvehiculo,
+    c.nombre,
+    c.precio,
+    DEREF(c.marca).nombre AS NombreMarca
+FROM Coche c
+ORDER BY NombreMarca;
+
+-- Ver colores de cada coche
+SELECT 
+    c.idvehiculo,
+    c.nombre,
+    c.precio,
+    col.COLUMN_VALUE AS COLOR
+FROM Coche c,
+    TABLE(c.colores) col;
+    
+-- Ver reparaciones de cada coche
+SELECT 
+    c.idvehiculo,
+    c.nombre,
+    c.precio,
+    rep.COLUMN_VALUE AS REPARACIONES
+FROM Coche c,
+    TABLE(c.reparaciones) rep;
+    
+-- Número de coches por marca
+SELECT 
+    DEREF(c.marca).nombre AS NombreMarca,
+    COUNT(*) AS NUM_COCHES
+FROM Coche c
+GROUP BY DEREF(c.marca).nombre;
+
+-- Diferentes marcas que existen (dice las marcas que hay sin que se dupliquen)
+SELECT DISTINCT(DEREF (c.marca).nombre) FROM Coche c;
+    
+-- Coches de una marca concreta(por nombre)
+SELECT 
+    c.idvehiculo,
+    c.nombre,
+    c.precio,
+    DEREF(c.marca).nombre AS NombreMarca
+FROM Coche c
+
+-- Colores del coche
+
+
+-- Reparaciones del coche 1
+
+
+
+
+
